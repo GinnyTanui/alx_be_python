02c5751b729a356ac39b2afed4c6793fd9e8f101
GinnyTanui <tanuiginny@gmail.com>
2025-05-24 16:54:46 +0300

    changes

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5  
+
+add = number1 + number2 
+subtract = number1 - number2 
+product = number1 * number2 
+
+print("Addition of 10 and 5 is", add) 
+print("Subtraction of 10 and 5 is",  subtract) 
+print("Addition of 10 and 5 is", add)

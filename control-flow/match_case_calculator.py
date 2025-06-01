3195846d6960f58e1ffe338e79d893abdeb53d45
GinnyTanui <tanuiginny@gmail.com>
2025-06-01 20:17:16 +0300

    aai

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,8 +1,6 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-
-operation = input("Choose the operation (+ , - , * , / ):")
-print()
+operation = input( "Choose the operation (+, -, *, /):") 
 
 match operation:
     case "+":

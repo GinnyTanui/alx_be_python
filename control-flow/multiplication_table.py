2b931818aa903ce63bc70bbff489c00402b7c78d
GinnyTanui <tanuiginny@gmail.com>
2025-06-01 19:04:16 +0300

    operation

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
-number = int(input("Enter a number to see its multiplication table: "))
+operation = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
-    print(f"{number} * {i} = {number * i}")
+    print(f"{operation} * {i} = {operation * i}")
 
-print(f"\nThat's the multiplication table for {number}. Hope it helps!")
+print(f"\nThat's the multiplication table for {operation}. Hope it helps!")

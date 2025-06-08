7263f4995e8ea2de8332789c99d2152786ce4fdf
GinnyTanui <tanuiginny@gmail.com>
2025-06-08 11:31:38 +0300

    sh

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,7 +6,7 @@ def display_menu():
     print("4. Exit") 
 
 def main():
-    shopping_list = ["Apples", "Bananas", "Carrots"]  # Initial shopping list
+    shopping_list = []  
     print("Welcome to the Shopping List Manager!")
     while True:
         display_menu() 

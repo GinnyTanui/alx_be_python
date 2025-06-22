3f6da78e6421fa48f936307dd9d4d4ec9cb13987
GinnyTanui <tanuiginny@gmail.com>
2025-06-22 13:55:50 +0300

    changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,23 +5,23 @@ class Library:
         self.books.append(book) 
     def list_books(self):
         for book in self.books:
-            info = f"{book.title} by {book.author}"
-            if hasattr(book, 'file_size'):
-                info += f" File Size: {book.file_size}"
-            elif hasattr(book, 'page_count'):
-                info += f", Page Count: {book.page_count}"
-            print(info)
-
+            print(book)
 
 class Book:
     def __init__(self, title, author):
         self.title = str(title) 
-        self.author = str(author)         
+        self.author = str(author)      
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"   
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)  
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"    
 class PrintBook(Book):
     def __init__(self, title, author,page_count):
         super().__init__(title, author)
-        self.page_count = int(page_count)
+        self.page_count = int(page_count) 
+    def __str__(self):
+        return f" PrintBook: {self.title} by {self.author}, PageCount: {self.page_count}"   

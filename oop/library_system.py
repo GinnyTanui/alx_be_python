c28ba1e6f8786f9cb86037d3736fe9d23ab45c84
GinnyTanui <tanuiginny@gmail.com>
2025-06-22 13:58:34 +0300

    changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,4 +24,4 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = int(page_count) 
     def __str__(self):
-        return f" PrintBook: {self.title} by {self.author}, PageCount: {self.page_count}"   
+       return f"PrintBook: {self.title} by {self.author}, PageCount: {self.page_count}"   
